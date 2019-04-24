ea10ed24ed7b086b92533026af144dafb8bcc118
Jennifer Powell <jerpowel321@gmail.com>
2019-04-24 13:58:12 -0700

    update utils API axios requests

--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,11 +1,24 @@
 import axios from "axios";
+const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+const APIKEY = "+intitle&key=AIzaSyBRJY6cbFl0IW1cAy0kLLzhhnYwutwEJ8U";
 
 export default {
-    deleteBook: function (){
+  // Queries Google Books API
+    searchBooks: function(query) {
+        return axios.get(BASEURL + query + APIKEY);
+    },
+    getAllBooks(){
+        return axios.get("/api/books")
+    },
+    saveBook(){
+        return axios.post("/api/books")
+    },
+    getBook: function (id){
+        return axios.get("/api/books/:id")
+    },
+    deleteBook: function (id){
         return axios.delete("/api/books/:id")
     },
-    getBooks(){
-        return axios.get("/api/books")
-    }
+   
+};
 
-}
