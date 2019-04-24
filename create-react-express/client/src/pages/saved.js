64caab31429abcc5d45a85086bdf7816f38ee814
ctghadiri <ctghadiri@gmail.com>
2019-04-24 13:32:31 -0700

    add deleteBook and getBooks functions to API.js and saved.js

--- create-react-express/client/src/pages/saved.js
+++ create-react-express/client/src/pages/saved.js
@@ -8,8 +8,31 @@ class saved extends Component{
 
     state={
         results =[],
+        title: "",
+        author: "",
+        description: "",
+        img: "",
+        link: ""
 
     }
+
+    componentDidMount() {
+        this.loadBooks();
+      }
+    
+      loadBooks = () => {
+        API.getBooks()
+          .then(res =>
+            this.setState({ results: res.data, title: "", author: "", description: "", img: "", link: "" })
+          )
+          .catch(err => console.log(err));
+      };
+    
+      deleteBook = id => {
+        API.deleteBook(id)
+          .then(res => this.loadBooks())
+          .catch(err => console.log(err));
+      };
 
     render(){
         return(
--- create-react-express/client/src/utils/API.js
+++ create-react-express/client/src/utils/API.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-export default{
-    
+export default {
+    deleteBook: function (){
+        return axios.delete("/api/books/:id")
+    },
+    getBooks(){
+        return axios.get("/api/books")
+    }
+
 }
