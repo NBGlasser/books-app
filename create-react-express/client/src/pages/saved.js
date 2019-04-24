bd28758cc33b49f1ceb3931d0290c1d50d64de10
ctghadiri <ctghadiri@gmail.com>
2019-04-24 13:09:38 -0700

    add render function and export

--- create-react-express/client/src/pages/saved.js
+++ create-react-express/client/src/pages/saved.js
@@ -1,11 +1,30 @@
 import React from "react";
 import DeleteBtn from "../components/DeleteBtn";
-import Form from "../components/Grid";
+import Grid from "../components/Grid";
 import List from "../components/List";
 import Nav from "../components/Nav";
 
 class saved extends Component{
 
+    state={
+        results =[],
 
-    
-}
+    }
+
+    render(){
+        return(
+            <Grid>
+                <Nav />
+                <div>
+                    {this.state.map( state.results)}
+                    <List className="data-block"></List>
+                
+                </div>
+            </Grid>
+        )
+
+    }
+
+};
+
+export default saved;
