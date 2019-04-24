e98d4c39ef902d49fafbb9d104714653f09ec326
ctghadiri <ctghadiri@gmail.com>
2019-04-24 12:39:45 -0700

    import components/react and define compnent

---
+++ create-react-express/client/src/pages/saved.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Form from "../components/Grid";
+import List from "../components/List";
+import Nav from "../components/Nav";
+
+class saved extends Component{
+
+
+    
+}
