b04d63a1a70a956eed09db36149c135b5bb0d467
Jennifer Powell <jerpowel321@gmail.com>
2019-04-25 13:17:14 -0700

    update grid folder

--- create-react-express/client/src/components/Grid/index.js
+++ create-react-express/client/src/components/Grid/index.js
@@ -1,10 +1,3 @@
-import React from "react";
-
-function Grid(children) {
-    return (<div>
-        {children}
-    </div>
-    )
-}
-
-export default Grid;
+export * from "./Col";
+export * from "./Container";
+export * from "./Row";
